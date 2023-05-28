8873d0c25d22b9673b96fc1a6221b7c98c8b5a1d
Chukwudi Ngwobia <ngwobiachukwudi@gmail.com>
2023-05-28 23:50:30 +0100

    place a note about zsh

--- install.sh
+++ install.sh
@@ -30,6 +30,10 @@ stow fonts
 # add zsh to valid login shells
 command -v zsh | sudo tee -a /etc/shells
 
+# NOTE: looks like command -v zsh is pointing to the default zsh that comes with macos /bin/zsh
+# if we are hardcore about the nix version, a workaround will be to access the nix zsh binary directly
+# $HOME/.nix-profile/bin/zsh | sudo tee -a /etc/shells
+
 # use zsh as the default shell
 sudo chsh -s $(which zsh) $USER
 
